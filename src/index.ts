0699d74e51aac3566ba01ebcd1382741d2f1a5bc
Konotorii <konotoribusiness@gmail.com>
2022-11-19 17:33:33 -0500

    TS Init

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import * as fs from "fs";
+import * as path from "path";
+import { DateTime } from 'luxon';
+import { join } from 'path';
+import { parse } from 'csv-parse';
+import delay from 'delay';
+import { readFileSync, writeFileSync, promises as fsPromises } from 'fs';
+
+async function main() {
+    
+}
+
+main()
